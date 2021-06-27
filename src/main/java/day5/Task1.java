00e9e45b17a14316c640b5169a4e25c9f6f7c57d
Pavel Titov <pvtitov@list.ru>
2021-06-27 16:49:08 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,13 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("blue");
+        car.setModel("Q3");
+        car.setYear(2010);
 
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(1980, "111", "asd1223");
 
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getModel());
+        System.out.println(motorbike.getYear());
     }
 }
