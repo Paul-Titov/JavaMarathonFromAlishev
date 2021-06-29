ba9d8f82fc007c0f74d38d4a632ea4c3014ecb4d
Pavel Titov <pvtitov@list.ru>
2021-06-30 00:56:16 +0300

    complete day7 tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane firstAirplane = new Airplane("Boing", 1990, 101, 100);
+        Airplane secondAirplane = new Airplane("Boing", 1990, 101, 100);
+        Airplane.compareAirplanes(firstAirplane, secondAirplane);
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,39 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random1 = new Random();
+        Random random2 = new Random();
+
+        Player.info();
+
+        Player player1 = new Player(random1.nextInt(11)+90);
+        Player player2 = new Player(random1.nextInt(11)+90);
+        Player player3 = new Player(random1.nextInt(11)+90);
+        Player player4 = new Player(random1.nextInt(11)+90);
+        Player player5 = new Player(random1.nextInt(11)+90);
+        Player.info();
+
+        Player player6 = new Player(random1.nextInt(11)+90);
+
+        Player.info();
+
+        Player player7 = new Player(random1.nextInt(11)+90);
+        Player player8 = new Player(random1.nextInt(11)+90);
+
+        Player.info();
+
+        for (int i = 0; i < 100; i++) {
+            player3.run();
+        }
+
+        Player.info();
+
+        Player player9 = new Player(random1.nextInt(11)+90);
+
+        Player.info();
 
     }
 }
