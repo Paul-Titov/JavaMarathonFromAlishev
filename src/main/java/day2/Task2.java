5ad0d5d5434897b5b5db2646eaaba37b34a58600
Pavel Titov <pvtitov@list.ru>
2021-07-02 01:31:42 +0300

    fixed day 2 task 2

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -11,7 +11,7 @@ public class Task2 {
             System.out.println("Некорректный ввод");
         }
         for (int i = firstNum+1; i < secondNum; i++) {
-            if (i%5==0 && !(i%10==0)) {
+            if (i%5==0 && (i%10!=0)) {
                 System.out.print(i + " ");
             }
         }
