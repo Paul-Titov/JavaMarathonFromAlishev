8180ba9271e19caff4d1fe9233235a6d7669b57c
Pavel Titov <pvtitov@list.ru>
2021-07-26 02:30:31 +0300

    fixed day 12 task2

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -8,10 +8,15 @@ public class Task2 {
 //        2. Создать новый список, заполнить его четными числами от 0 до 30 и от 300 до 350. Вывести список.
         List<Integer> list  = new ArrayList<>();
         for (int i = 0; i <= 30; i++) {
-            list.add(i);
+            if (i % 2 == 0) {
+                list.add(i);
+            }
+
         }
         for (int i = 300; i <= 350; i++) {
-            list.add(i);
+            if (i % 2 == 0) {
+                list.add(i);
+            }
         }
 
         System.out.println(list);
