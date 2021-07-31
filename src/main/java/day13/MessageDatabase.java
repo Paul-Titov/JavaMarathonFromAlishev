0424ad9f5b396c4bd02ef60456543914e49e72f4
Pavel Titov <pvtitov@list.ru>
2021-08-01 01:34:36 +0300

    fixed day 13 task

--- src/main/java/day13/MessageDatabase.java
+++ src/main/java/day13/MessageDatabase.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class MessageDatabase {
-    static List<Message> messages = new ArrayList<>();
+    private static List<Message> messages = new ArrayList<>();
 
     public static void addNewMessage(User sender, User receiver, String text) {
         messages.add(new Message(sender, receiver, text));
@@ -18,10 +18,10 @@ public class MessageDatabase {
     public static void showDialog(User u1, User u2) {
         List<Message> dialog = new LinkedList<>();
         for (Message message : messages) {
-            if ((message.receiver == u1 && message.sender == u2) || (message.receiver == u2 && message.sender == u1)) {
+            if ((message.getReceiver() == u1 && message.getSender() == u2) || (message.getReceiver() == u2 && message.getSender() == u1)) {
                 dialog.add(message);
             }
         }
-        dialog.forEach((Message m)->System.out.println(m.sender+": "+m.text));
+        dialog.forEach((Message m)->System.out.println(m.getSender()+": "+m.getText()));
     }
 }
